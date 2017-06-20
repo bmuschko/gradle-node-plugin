865abfcf3f2bc3f3c0800a3efe888853cca0e6cb
Eric Wendelin <eric@gradle.com>
2017-06-20 11:22:41 -0700

    Add ideas for user convenience

--- src/main/java/org/gradle/plugins/node/base/NodeBasePlugin.java
+++ src/main/java/org/gradle/plugins/node/base/NodeBasePlugin.java
@@ -1,10 +1,10 @@
 package org.gradle.plugins.node.base;
 
-import org.gradle.plugins.node.base.tasks.npm.NpmCommand;
-import org.gradle.plugins.node.base.tasks.npm.NpmInit;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.plugins.node.base.tasks.npm.NpmCommand;
+import org.gradle.plugins.node.base.tasks.npm.NpmInit;
 
 /**
  * A plugin providing basic infrastructure for managing <a href="https://nodejs.org">Node.js</a> applications.
@@ -30,8 +30,32 @@ public class NodeBasePlugin implements Plugin<Project> {
             }
         });
 
+        // TODO: generateTasksFromPackageJson(project, project.file("package.json"));
+        // TODO: generateTasksFromNodeModules(project, getNodeModulesDirectory(project));
+
         // TODO: Download and install Node.js automatically
         // TODO: Fails the build if Node.js cannot be detected on the machine
         // TODO: Maybe detect information about the Node environment via some sort of reporting task?
     }
+
+    // Generate tasks from package.json scripts
+//    private void generateTasksFromPackageJson(Project project, File packageJsonFile) {
+//        def packageJson = packageJsonFile.withReader { r -> new groovy.json.JsonSlurper().parse(r) }
+//        if (packageJson.scripts) {
+//            for (Map.Entry<String, String> script : packageJson.scripts) {
+//                // TODO: use value for arguments, use key for configuration
+//                project.tasks.create(name: NODE_RUN_PREFIX + script.key, type:NodeExec);
+//            }
+//        }
+//    }
+//
+//    // Generate tasks from node_modules/.bin/
+//    private void generateTasksFromNodeModules(Project project, File nodeModulesDir) {
+//        // List files in node_modules/.bin
+//        for (File file : new File(nodeModulesDir, ".bin").listFiles()) {
+//            if (file.isFile()) {
+//                project.tasks.create(name: NODE_BIN_PREFIX + file.getName(), type: NodeExec);
+//            }
+//        }
+//    }
 }
